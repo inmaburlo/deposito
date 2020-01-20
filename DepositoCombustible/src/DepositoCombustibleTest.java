55424f3cd4e132817d94262e73703f2b3f0189a8
Inma_Burlo <inmaburlo@gmail.com>
2020-01-20 23:45:35 +0100

    3 casos diferentes

--- DepositoCombustible/src/DepositoCombustibleTest.java
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -5,13 +5,23 @@ import org.junit.Test;
 public class DepositoCombustibleTest {
 
 	@Test
-	public final void testDepositoLlenoMitad() {
+	public final void testDepositoConsumirMitad() {
 		DepositoCombustible deposito = new DepositoCombustible(12, 0);
 		deposito.consumir(6);
 		assertEquals(deposito.getDepositoMax()/2,6,1);
-		
+	}
+	
+	@Test
+	public final void testDepositoConsumirCompleto() {
 		DepositoCombustible deposito2 = new DepositoCombustible(12, 12);
 		deposito2.consumir(12);
 		assertEquals(deposito2.getDepositoNivel(),0,1);
+	}
+	
+	@Test
+	public final void testDepositoConsumirNegativo() {
+		DepositoCombustible deposito2 = new DepositoCombustible(12, 12);
+		deposito2.consumir(-12);
+		assertEquals(deposito2.getDepositoNivel(),24,1);
 	}
 }
