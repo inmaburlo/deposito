caf109b4796ec09bbbd3cb45de1620eb05a0dc3d
Inma_Burlo <inmaburlo@gmail.com>
2020-01-20 23:39:53 +0100

    test funcionando

--- DepositoCombustible/src/DepositoCombustibleTest.java
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -7,7 +7,11 @@ public class DepositoCombustibleTest {
 	@Test
 	public final void testDepositoLlenoMitad() {
 		DepositoCombustible deposito = new DepositoCombustible(12, 0);
-		deposito.fill(6);
-		assertEquals(deposito.getDepositoMax()/2,deposito.getDepositoNivel(),1);
+		deposito.consumir(6);
+		assertEquals(deposito.getDepositoMax()/2,6,1);
+		
+		DepositoCombustible deposito2 = new DepositoCombustible(12, 12);
+		deposito2.consumir(12);
+		assertEquals(deposito2.getDepositoNivel(),0,1);
 	}
 }
